ee2ed8030f1dc75ea1ba1f38c2bbd07a0f83ada5
gpelayo <gpelayo109@gmail.com>
2022-07-29 20:36:42 -0700

    Refactored scraping modules

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ import json
 import re
 from urllib.parse import urlencode, urlparse, urlunparse
 
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 from exceptions import NoResultsException
@@ -73,24 +74,27 @@ class IMDBSeleniumScraper:
         self.browser.get(url)
 
         try:
-            title_section = self.browser.find_element_by_xpath('//div[@class="findSection"]/h3/a[@name="tt"]/../..')
+            title_div = self.browser.find_element(By.XPATH, '//div[@class="findSection"]/h3/a[@name="tt"]/../..')
         except NoSuchElementException:
             raise NoResultsException(f'{title} has no results in IMDB')
 
-        episode_element = title_section.find_elements_by_xpath('//tr/td[@class="result_text"]/a')[0]
-        show_url = episode_element.get_attribute('href')
+        show_result_element = title_div.find_elements(By.XPATH, '//tr/td[@class="result_text"]/a')[0]
+        show_url = show_result_element.get_attribute('href')
         series_id = re.findall(r'(?<=title/)tt.+(?=/\?ref)', show_url)[0]
-        series = Series(series_id, episode_element.text)
+        series = Series(series_id, show_result_element.text)
         if 'title/tt' in show_url:
-            thumb_links = episode_element.find_elements_by_xpath('../..//td[@class="primary_photo"]/a/img')
+            thumb_links = show_result_element.find_elements(By.XPATH, '../..//td[@class="primary_photo"]/a/img')
             series.thumbnail_url = thumb_links[0].get_attribute('src') if thumb_links else None
+        return series
+
+    def scrape_series_page(self, series):
         season_link = f'http://www.imdb.com/title/{series.series_id}/episodes'
         series.trivia_set += self.extract_trivia_page(f'https://www.imdb.com/title/{series.series_id}/trivia')
         self.browser.get(season_link)
-        series.season_count = len(self.browser.find_elements_by_xpath('//select[@id="bySeason"]/option'))
+        series.season_count = len(self.browser.find_elements(By.XPATH, '//select[@id="bySeason"]/option'))
         for season in range(1, series.season_count+1):
             self.browser.get(season_link + f'?season={season}')
-            for ep_element in self.browser.find_elements_by_xpath('//strong/a[@itemprop="name"]'):
+            for ep_element in self.browser.find_elements(By.XPATH, '//strong/a[@itemprop="name"]'):
                 ep_url = ep_element.get_attribute('href')
                 ep_id = re.findall(r'(?<=title/)tt.+(?=/\?ref)', ep_url)[0]
                 new_ep = Episode(ep_id, ep_element.text, season)
@@ -104,7 +108,7 @@ class IMDBSeleniumScraper:
     def extract_trivia_page(self, url: str) -> List[Trivia]:
         trivia_set = []
         self.browser.get(url)
-        trivia_divs = self.browser.find_elements_by_xpath('//div[contains(@id,"tr")]/div[@class="sodatext"]')
+        trivia_divs = self.browser.find_elements(By.XPATH, '//div[contains(@id,"tr")]/div[@class="sodatext"]')
         for trivia_div in trivia_divs:
             trivia_id = hashlib.md5((trivia_div.text+url).encode('utf-8')).hexdigest()
             tr = Trivia(trivia_id, trivia_div.text)
